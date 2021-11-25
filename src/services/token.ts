6e0c5fcf7cf38a566f993797455e14e9ecd17fe9
oluchioraekwe <oluchi.oraekwe@decagon.dev>
2021-11-25 18:33:21 +0100

    token edited

--- src/services/token.ts
+++ src/services/token.ts
@@ -1,15 +1,9 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { obj } from "./interface";
 
 dotenv.config();
 
 const secreteKey = process.env.SECRET_KEY as string;
-
-// export const generateToken = (value: obj): string => {
-//   const token = jwt.sign(value, secreteKey, { expiresIn: "100m" });
-//   return token;
-// };
 export const generateToken = (email: string, id: number): string => {
   const token = jwt.sign({ email, id }, secreteKey, { expiresIn: "100m" });
   return token;
